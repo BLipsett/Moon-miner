6b0f2e51b2b3cc7621589a6fbd918ad659fdc835
Brian Lipsett <lipsettbrian3@gmail.com>
2021-05-14 14:53:34 -0600

    oxygen interval counter

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let cheeseCount = 0;
 let oxygenLevel = 100;
+let gametTime = 4000;
+
+let upgrades = [{}];
 
 function mineCheese() {
   cheeseCount++;
@@ -10,7 +13,6 @@ function mineCheese() {
 function drawBoard() {
   document.getElementById("cheese-money").innerText = cheeseCount;
 
-  document.getElementById("oxygen-level").innerText = oxygenLevel;
   if (cheeseCount > 10) {
     let hammer = document.getElementById("moon-hammer");
 
@@ -21,6 +23,26 @@ function drawBoard() {
 
 function moonHammer() {
   cheeseCount += 5;
+}
+
+function oxygenBar() {
+  oxygenLevel -= 2;
+  document.getElementById("oxygen-level").innerText = oxygenLevel;
+}
+
+function startInterval() {
+  document.getElementById("start-button").classList.add("d-none");
+  let countdown = setInterval(oxygenBar, 1000);
+  console.log("minus 2 oxygen", countdown);
+}
+
+// function startGame() {
+//   setTimeout(gameOver, gametTime);
+//   oxygen();
+// }
+
+function gameOver() {
+  alert("Game Over");
 }
 
 drawBoard();
